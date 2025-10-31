dee084c18a92c166b15ebf1963e546432d7e6d48
kaverikesharwani <kaverikesharwani418@gmail.com>
2025-10-31 18:24:07 +0530

    Added Tailwind props project

---
+++ 03tailwindprops/tailwind.config.js
@@ -0,0 +1,10 @@
+/**@type {import('tailwindcss').config} */
+export default{
+    content: ["./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme:{
+        extend: {},
+    },
+    pluggins: [],
+}
